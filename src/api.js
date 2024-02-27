9be9e949c543db913ec8cb05267caa0bf80f28d6
himzei <himzei@gmail.com>
2024-02-27 09:27:20 +0900

    0227

--- src/api.js
+++ src/api.js
@@ -1,16 +1,15 @@
+const API_URL = process.env.REACT_APP_API_URL;
+
 export async function apiPostNoticeWrite(formData) {
   try {
     // console.log("API", formData);
-    return await fetch(
-      "https://straightforward-berri-himzei.koyeb.app/notice/write",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formData),
-      }
-    ).then((res) => res.json());
+    return await fetch(`${API_URL}/notice/write`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    }).then((res) => res.json());
   } catch (error) {
     console.log(error);
   }
